e7801416c1ee74ab5d25d904f0989b4abccef48d
faith mitei <faithmitey@gmail.com>
2022-12-16 12:44:08 +0300

    halfway through the  test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+const element = document.getElementById("main");
+
+document.querySelector('main').remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.append('li')
+
+
+newHeader.textContent = `faith is the champion"`;
+ console.log(newHeader.textContent)
+
+ 
